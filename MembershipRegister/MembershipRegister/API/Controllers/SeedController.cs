86bca7b77f1eb459d24aa043b3b86fd2ae4b3348
Dani-dvp <kevin1995dani@gmail.com>
2022-02-20 11:12:31 +0100

    Altered small stuff to prevent seed from adding the same stuff multiple times

--- MembershipRegister/MembershipRegister/API/Controllers/SeedController.cs
+++ MembershipRegister/MembershipRegister/API/Controllers/SeedController.cs
@@ -16,48 +16,52 @@ namespace MembershipRegister.API.Controllers
         [HttpPost("competitions")]
         public async Task<string> SeedCompetitionsToDatabase()
         {
-            var competition1 = new Competition { name = "Map race", description = "Collecting pins in the forest using a map", winner = "Kevin Dani" };
-            var competition2 = new Competition { name = "Basketball", description = "Basketball tournament with 12 teams", winner = "Team Hulk" };
-            var competition3 = new Competition { name = "Beach Volleyball", description = "Volleyball tournament with 9 teams", winner = "Team Lokomotiv" };
+            if (!_membershipDbContext.Competitions.Any()) {
+                var competition1 = new Competition { name = "Map race", description = "Collecting pins in the forest using a map", winner = "Kevin Dani" };
+                var competition2 = new Competition { name = "Basketball", description = "Basketball tournament with 12 teams", winner = "Team Hulk" };
+                var competition3 = new Competition { name = "Beach Volleyball", description = "Volleyball tournament with 9 teams", winner = "Team Lokomotiv" };
 
 
-            await _membershipDbContext.Competitions.AddAsync(competition1);
-            await _membershipDbContext.Competitions.AddAsync(competition2);
-            await _membershipDbContext.Competitions.AddAsync(competition3);
+                await _membershipDbContext.Competitions.AddAsync(competition1);
+                await _membershipDbContext.Competitions.AddAsync(competition2);
+                await _membershipDbContext.Competitions.AddAsync(competition3);
 
-            _membershipDbContext.SaveChanges();
+                _membershipDbContext.SaveChanges();
+            }
             return "Created Seed";
         }
 
         [HttpPost("admin")]
         public async Task<string> SeedAdminsToDatabase()
         {
-            var admin = new Administrator { userName = "admin", password = "password" };
+            if (!_membershipDbContext.Administrators.Any()) {
+                var admin = new Administrator { userName = "admin", password = "password" };
 
-            await _membershipDbContext.Administrators.AddAsync(admin);
+                await _membershipDbContext.Administrators.AddAsync(admin);
 
-            _membershipDbContext.SaveChanges();
-
+                _membershipDbContext.SaveChanges();
+            }
             return "Created Seed";
         }
 
         [HttpPost("customers")]
         public async Task<string> SeedCustomersToDatabase()
         {
-            var customer1 = new Customer { firstName = "Kevin", lastName = "Dani", paidFor = "Year" };
-            var customer2 = new Customer { firstName = "Helena", lastName = "Boström", paidFor = "Month" };
-            var customer3 = new Customer { firstName = "Johan", lastName = "Klum", paidFor = "Half year" };
-            var customer4 = new Customer { firstName = "Malin", lastName = "Andersson", paidFor = "Quarter" };
-            var customer5 = new Customer { firstName = "Henrik", lastName = "Svensson", paidFor = "Not paid" };
+            if (!_membershipDbContext.Customers.Any()) {
+                var customer1 = new Customer { firstName = "Kevin", lastName = "Dani", paidFor = "Year" };
+                var customer2 = new Customer { firstName = "Helena", lastName = "Boström", paidFor = "Month" };
+                var customer3 = new Customer { firstName = "Johan", lastName = "Klum", paidFor = "Half year" };
+                var customer4 = new Customer { firstName = "Malin", lastName = "Andersson", paidFor = "Quarter" };
+                var customer5 = new Customer { firstName = "Henrik", lastName = "Svensson", paidFor = "Not paid" };
 
-            await _membershipDbContext.Customers.AddAsync(customer1);
-            await _membershipDbContext.Customers.AddAsync(customer2);
-            await _membershipDbContext.Customers.AddAsync(customer3);
-            await _membershipDbContext.Customers.AddAsync(customer4);
-            await _membershipDbContext.Customers.AddAsync(customer5);
+                await _membershipDbContext.Customers.AddAsync(customer1);
+                await _membershipDbContext.Customers.AddAsync(customer2);
+                await _membershipDbContext.Customers.AddAsync(customer3);
+                await _membershipDbContext.Customers.AddAsync(customer4);
+                await _membershipDbContext.Customers.AddAsync(customer5);
 
-            _membershipDbContext.SaveChanges();
-
+                _membershipDbContext.SaveChanges();
+            }
             return "Created Seed";
         }
     }
